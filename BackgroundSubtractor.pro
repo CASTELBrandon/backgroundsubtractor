8f9544781590445eca5d5785aca3b1eca8fc6d1b
brand <brand@DESKTOP-OU4FDOE>
2021-09-21 02:01:43 +0200

    Initialize the command line system

--- BackgroundSubtractor.pro
+++ BackgroundSubtractor.pro
@@ -2,7 +2,7 @@ QT       += core gui
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
-CONFIG += c++11
+CONFIG += c++11 console
 
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
@@ -17,6 +17,15 @@ HEADERS += \
 
 TRANSLATIONS += \
     BackgroundSubtractor_en_150.ts
+
+INCLUDEPATH += C:\OpenCV\opencv\build\include
+
+LIBS += C:\OpenCV\opencv\build\bin\libopencv_core453.dll
+LIBS += C:\OpenCV\opencv\build\bin\libopencv_highgui453.dll
+LIBS += C:\OpenCV\opencv\build\bin\libopencv_imgcodecs453.dll
+LIBS += C:\OpenCV\opencv\build\bin\libopencv_imgproc453.dll
+LIBS += C:\OpenCV\opencv\build\bin\libopencv_features2d453.dll
+LIBS += C:\OpenCV\opencv\build\bin\libopencv_calib3d453.dll
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- main.cpp
+++ main.cpp
@@ -1,11 +1,50 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QCommandLineParser>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
+    // Init application
     QApplication a(argc, argv);
+    QApplication::setApplicationName("BackgroundSubtractor");
+    QApplication::setApplicationVersion("1.0");
     MainWindow w;
-    w.show();
+
+    // Check if we have command line arguments
+    auto args = a.arguments();
+    args.removeAt(0); // The first argument is the executable path
+    if(!args.empty()){
+        QCommandLineParser parser;
+        parser.setApplicationDescription("This program allows to subtract the background of a sequence of images.");
+        parser.addHelpOption();
+        parser.addVersionOption();
+
+        QCommandLineOption inputOption(QStringList() << "i" << "input", "Input folder which contains multiple sequences of camera images.", "path");
+        parser.addOption(inputOption);
+
+        QCommandLineOption outputOption(QStringList() << "o" << "output", "Output folder which will contain all the subtracted images.", "path");
+        parser.addOption(outputOption);
+
+        parser.process(a);
+
+        // Check if input and output folder are set
+        if(!(parser.isSet(inputOption) && parser.isSet(outputOption))){
+            qCritical() << "Error : if you are using the command line system, the input path and the output path must be specified.";
+            return -1;
+        }
+
+        QString input = parser.value(inputOption);
+        qDebug() << "Input folder : " << input;
+
+        QString output = parser.value(outputOption);
+        qDebug() << "Output folder : " << output;
+    }
+    // If no command lines we show the window
+    else{
+        w.show();
+    }
+
     return a.exec();
 }
